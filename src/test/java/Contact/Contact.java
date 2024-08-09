1d598bebcda1e2484c9e8e8941eb0a6dc25c926c
razamd260 <razamd260@gmail.com>
2024-08-09 08:24:42 +0530

    8:23

--- src/test/java/Contact/Contact.java
+++ src/test/java/Contact/Contact.java
@@ -8,16 +8,26 @@ public class Contact extends BaseClass{
      
 	@Test(groups = "smoke")
 	public void clickContect() {
-		String smoke=System.getProperty("smoke");
-		System.out.println(smoke);
+		String Browser=System.getProperty("Browser");
+		System.out.println("browser Passing from  cmd   :  ===>  "+Browser);
+		String regression_Suit=System.getProperty("regression");
+		System.out.println("regression Passing from  cmd   :  ===>  "+regression_Suit);
+		String evn=System.getProperty("evn");
+		System.out.println("evn Passing from  cmd   :  ===>  "+evn);
 		System.out.println(" Smoke Testing ====> Click On Contact module");
+		
+		System.out.println(" ==========  8 : 23 ==============");
 	}
 	
 	
 	@Test(groups = "regression")
 	public void CreateContect() {
-		String regression=System.getProperty("regression");
-		System.out.println(regression);
+		String Browser=System.getProperty("Browser");
+		System.out.println("browser Passing from  cmd   :  ===>  "+Browser);
+		String regression_Suit=System.getProperty("regression");
+		System.out.println("regression Passing from  cmd   :  ===>  "+regression_Suit);
+		String evn=System.getProperty("evn");
+		System.out.println("evn Passing from  cmd   :  ===>  "+evn);
 		System.out.println(" regression Testing ====> Create Contact module");
 	}
 }
--- src/test/java/Org/OrgCreate.java
+++ src/test/java/Org/OrgCreate.java
@@ -9,16 +9,24 @@ public class OrgCreate extends BaseClass{
 	@Test(groups = "smoke")
 	public void clickOrg() {
 		
-		String smoke=System.getProperty("smoke");
-		System.out.println(smoke);
+		String Browser=System.getProperty("Browser");
+		System.out.println("browser Passing from  cmd   :  ===>  "+Browser);
+		String regression_Suit=System.getProperty("regression");
+		System.out.println("regression Passing from  cmd   :  ===>  "+regression_Suit);
+		String evn=System.getProperty("evn");
+		System.out.println("evn Passing from  cmd   :  ===>  "+evn);
 		System.out.println("Smoke Testing ===>> Click Org");
 	}
 	
 	
 	@Test(groups = "regression")
 	public void createOrg() {
-		String regression=System.getProperty("regression");
-		System.out.println(regression);
+		String Browser=System.getProperty("Browser");
+		System.out.println("browser Passing from  cmd   :  ===>  "+Browser);
+		String regression_Suit=System.getProperty("regression");
+		System.out.println("regression Passing from  cmd   :  ===>  "+regression_Suit);
+		String evn=System.getProperty("evn");
+		System.out.println("evn Passing from  cmd   :  ===>  "+evn);
 		
 		System.out.println("Resgression Testing ===>> createOrg");
 	}
