6535d470cbbdf08a16726816082dbbb489cd6ecf
Graza4645 <graza4645@gmail.com>
2024-08-12 10:52:42 +0530

    Noushad branch

---
+++ src/test/java/Help/Snippet.java
@@ -0,0 +1,37 @@
+package Help;
+
+import org.testng.annotations.Test;
+
+public class Snippet {
+	@Test(groups = "smokeTest")
+		public void clickHelp() {
+			
+			String Browser=System.getProperty("Browser");
+			System.out.println("browser Passing from  cmd   :  ===>  "+Browser);
+			String regression_Suit=System.getProperty("regression");
+			System.out.println("regression Passing from  cmd   :  ===>  "+regression_Suit);
+			String evn=System.getProperty("env");
+			System.out.println("evn Passing from  cmd   :  ===>  "+evn);
+			System.out.println("Smoke Testing ===>> Click Org");
+			System.out.println(" Noushad ansari Smolke Testing Done");
+		
+		}
+		
+		
+		@Test(groups = "regressionTest")
+		public void createHelp() {
+			String Browser=System.getProperty("Browser");
+			System.out.println("browser Passing from  cmd   :  ===>  "+Browser);
+			String regression_Suit=System.getProperty("regression");
+			System.out.println("regression Passing from  cmd   :  ===>  "+regression_Suit);
+			String evn=System.getProperty("env");
+			System.out.println("evn Passing from  cmd   :  ===>  "+evn);
+			
+			System.out.println("Resgression Testing ===>> createOrg");
+			System.out.println("Noushad Ansari resgression Testing");
+			System.out.println("Monday Noushad Ansari resgression Testing");
+	
+}
+		
+}
+
